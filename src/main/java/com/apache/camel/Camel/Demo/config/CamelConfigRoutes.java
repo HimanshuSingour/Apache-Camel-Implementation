5b15989cb8090744b947028225eb9fac7122e9a9
HimanshuSingour <Himanshu>
2023-12-08 22:00:05 +0530

    Code Logic Change

--- src/main/java/com/apache/camel/Camel/Demo/config/CamelConfigRoutes.java
+++ src/main/java/com/apache/camel/Camel/Demo/config/CamelConfigRoutes.java
@@ -1,5 +1,6 @@
 package com.apache.camel.Camel.Demo.config;
 
+import com.apache.camel.Camel.Demo.process.MessageProcessor;
 import com.apache.camel.Camel.Demo.process.impl.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.LoggingLevel;
@@ -13,18 +14,41 @@ import org.springframework.context.annotation.Configuration;
 public class CamelConfigRoutes extends RouteBuilder {
 
     @Autowired
-    private BrowseCarFlagProcess browseCarFlagProcess;
+    private GetStudentByNameAndEmailProcess getStudentByNameAndEmailProcess;
+
+    @Autowired
+    private GetStudentProcess getStudentProcess;
+
+    @Autowired
+    private MessageOccureProcess messageOccureProcess;
+
+    @Autowired
+    private SaveStudentProcess saveStudentProcess;
 
 
     @Override
     public void configure() {
 
 
-        from("direct:getBrowse")
-                .routeId("browse")
-                .bean(browseCarFlagProcess)
+        from("direct:saveEmailName")
+                .routeId("save_by_email_and_name")
+                .bean(getStudentByNameAndEmailProcess)
                 .end();
 
+        from("direct:getStudent")
+                .routeId("get_student")
+                .bean(getStudentProcess)
+                .end();
+
+        from("direct:getMessage")
+                .routeId("get_message")
+                .bean(messageOccureProcess)
+                .end();
+
+        from("direct:saveStudent")
+                .routeId("save_student")
+                .bean(saveStudentProcess)
+                .end();
 
 
     }
--- src/main/java/com/apache/camel/Camel/Demo/service/CarBrowseService.java
+++
@@ -1,6 +0,0 @@
-package com.apache.camel.Camel.Demo.service;
-
-public interface CarBrowseService {
-
-    CarFlagResponseDTO carFlagBrowse(String eqpId, String stn333, String stnSt);
-}
